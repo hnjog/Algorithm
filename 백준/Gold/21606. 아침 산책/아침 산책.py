c7684e1f25106d8e377da2d725994063a7291202
hnjog <43630972+hnjog@users.noreply.github.com>
2023-10-21 22:18:56 +0900

    [Gold III] Title: 아침 산책, Time: 764 ms, Memory: 79492 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,5 +1,6 @@
 import sys
-input = sys.stdin.readline
+
+sys.setrecursionlimit(10**8)
 
 # DFS
 # 다만 아까처럼 Color 를 이용 (이분 그래프)
@@ -7,39 +8,39 @@ input = sys.stdin.readline
 # 산책 경로에 그 외의 '실내' 장소는 없어야 함
 # 서로 다른 산책 경로의 개수
 
-# 일단 중간에 '실외'가 반드시 들어갈 필요는 없음
+# start가 inOut[start]의 '실내'인 경우에만 되도록 밖에서 넣어줘야 함
+def dfs(start,inOut,child):
+    visit = set()
+    stack = [start]
 
-vNum = int(input().strip()) # 정점의 개수
+    res = 0
+    while stack:
+        node = stack.pop()
+        visit.add(node)
+
+        for neighbor in child[node]:
+            if neighbor not in visit:
+                if inOut[neighbor] == '1':
+                    res += 1
+                else:
+                    stack.append(neighbor)
+
+    return res
+
+vNum = int(sys.stdin.readline().strip()) # 정점의 개수
 eNum = vNum - 1
-inOut = input().strip()     # 각 정점의 실내/실외 정보 (1 : 실내, 0 : 실외)
+inOut = sys.stdin.readline().strip()     # 각 정점의 실내/실외 정보 (1 : 실내, 0 : 실외)
 
-visit = [False] * vNum
 child = [[] for _ in range(vNum)]
 for i in range(eNum):
-    start,to = map(int,input().split())
+    start,to = map(int,sys.stdin.readline().split())
     child[start-1].append(to-1)
     child[to-1].append(start-1)
 
 result = 0
 
-# start가 inOut[start]의 '실내'인 경우에만 되도록 밖에서 넣어줘야 함
-def dfs(start):
-    visit[start] = True
-
-    for i in range(len(child[start])):
-        if visit[child[start][i]] == True:
-            continue
-
-        if inOut[child[start][i]] == '1':
-            global result
-            result += 1
-            continue
-
-        dfs(child[start][i])
-
 for i in range(vNum):
     if inOut[i] == '1':
-        visit = [False] * vNum
-        dfs(i)
+        result += dfs(i,inOut,child)
 
 print(result)
