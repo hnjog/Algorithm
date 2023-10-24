ca9f219d3e6e55f6c15726c453d011b932ee09c4
hnjog <43630972+hnjog@users.noreply.github.com>
2023-10-24 22:20:52 +0900

    [Gold IV] Title: 빙산, Time: 708 ms, Memory: 161040 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -2,26 +2,27 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-d = [[-1,0],[1,0],[0,1],[0,-1]]
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
 
-def bfs(y,x):
+def bfs(startY,startX,visit):
     queue = deque()
-    queue.append([y,x])
-    visited[y][x] = True
+    queue.append([startY,startX])
+    visit[startY][startX] = True
 
     while queue:
-        r,c = queue.popleft()
+        y,x = queue.popleft()
         for i in range(4):
-            dr = r + d[i][0]
-            dc = c + d[i][1]
+            ny = y + dy[i]
+            nx = x + dx[i]
 
-            if (0<=dr<n) and (0<=dc<m):
-                if graph[dr][dc] == 0:
-                    visited[r][c] += 1
+            if (0<=ny<n) and (0<=nx<m):
+                if graph[ny][nx] == 0:
+                    visit[y][x] += 1
 
-                if not visited[dr][dc] and graph[dr][dc] != 0 :
-                    queue.append([dr,dc])
-                    visited[dr][dc] = True
+                if visit[ny][nx] == False and graph[ny][nx] != 0:
+                    queue.append([ny,nx])
+                    visit[ny][nx] = True
 
 
 n , m = map(int,input().split())
@@ -33,17 +34,21 @@ time = 0
 while True:
     count = 0
     visited = [[0] * m for _ in range(n)]
+
+    # bfs 돌며 근처에 0 확인 및
+    # 얼음이 두 덩어리인지 확인
     for i in range(n):
-        for k in range(m):
-            if not visited[i][k] and graph[i][k]!=0:
-                bfs(i,k)
+        for j in range(m):
+            # 0이 아니고, 방문 안했다
+            if not visited[i][j] and graph[i][j] !=0:
+                bfs(i,j,visited) #bfs로 탐색하기
                 count +=1
 
+    # 그래프에서 얼음 녹은만큼 빼주기
     for i in range(n):
-        for k in range(m):
-            if visited[i][k] :
-                graph[i][k] -= (visited[i][k]-1)
-                if graph[i][k] < 0: graph[i][k] = 0
+        for j in range(m):
+            if visited[i][j]:
+                graph[i][j] = max(graph[i][j] - visited[i][j] + 1,0)
 
     time += 1
     if count == 0:
