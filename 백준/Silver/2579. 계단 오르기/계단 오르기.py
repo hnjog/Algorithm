6978f193c73794dad95a78a3349bd720bcfefba2
hnjog <43630972+hnjog@users.noreply.github.com>
2023-11-05 16:13:44 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,18 @@
+import sys
+
+inp = sys.stdin.readline
+
+n = int(inp().strip())
+
+dp = [0 for _ in range(n + 1)]
+val = [0 for _ in range(n+1)]
+
+for i in range(1,n+1):
+    val[i] = int(inp().strip())
+
+dp[1] = val[1]
+
+for i in range(2,n+1):
+    dp[i] = max(dp[i-3] +val[i-1] ,dp[i-2]) + val[i]
+
+print(dp[n])
