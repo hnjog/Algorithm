e057c20eff705008b59177a485d01f5562175a40
hnjog <43630972+hnjog@users.noreply.github.com>
2024-05-28 11:32:55 +0900

    [level 4] Title: [3차] 자동완성, Time: 320.25 ms, Memory: 148 MB -BaekjoonHub

---
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> words) {
+    struct node
+    {
+        unordered_map<char, unique_ptr<node>> m;
+        int count = 0;
+
+        void insert(const string& str, int ind)
+        {
+            if (ind >= str.size())
+                return;
+
+            if (m.find(str[ind]) == m.end())
+            {
+                m[str[ind]] = make_unique<node>();
+            }
+
+            m[str[ind]]->count += 1;
+            m[str[ind]]->insert(str, ind + 1);
+        }
+
+        int findCount(const string& str, int ind)
+        {
+            if (ind >= str.size())
+                return ind;
+
+            if (m[str[ind]]->count == 1)
+                return ind + 1;
+
+            return m[str[ind]]->findCount(str, ind + 1);
+        }
+    };
+
+    int answer = 0;
+    node start;
+    for (const auto& word : words)
+    {
+        start.insert(word, 0);
+    }
+
+    for (const auto& word : words)
+    {
+        answer += min(static_cast<int>(word.size()), start.findCount(word, 0));
+    }
+
+    return answer;
+}
