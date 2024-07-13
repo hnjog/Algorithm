ae75c043be487930bae6a7573fa97f8b00938dd3
hnjog <43630972+hnjog@users.noreply.github.com>
2024-07-13 12:53:09 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.76 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -0,0 +1,113 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include<map>
+#include<string.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> fees, vector<string> records) {
+	vector<int> answer;
+	const char* delim = ": ";
+	struct timeInfo
+	{
+		int hour;
+		int min;
+		int fullMin;
+		bool isIn;
+	};
+
+	map < string, timeInfo> m;
+	for (auto& s : records)
+	{
+		int hour = -1;
+		int min = -1;
+		string key = "";
+		bool isIn = false;
+		bool isCheck[3] = { false, };
+
+		char* p_s = strtok((char*)s.c_str(), delim);
+		while (p_s != NULL)
+		{
+			if (hour == -1)
+			{
+				hour = stoi(p_s);
+			}
+			else if (min == -1)
+			{
+				min = stoi(p_s);
+			}
+			else if (key == "")
+			{
+				key = p_s;
+			}
+			else if (isIn == false)
+			{
+				if (strcmp(p_s, "IN") == 0)
+				{
+					isIn = true;
+				}
+			}
+
+			p_s = strtok(NULL, delim);
+		}
+
+		timeInfo t = { hour,min,0,isIn };
+		if (m.find(key) == m.end())
+			m[key] = t;
+		else
+		{
+			if (t.isIn == false)
+			{
+				int vH = t.hour - m[key].hour;
+				int vM = t.min - m[key].min;
+				int fV = vH * 60 + vM;
+				m[key].fullMin += fV;
+				m[key].hour = t.hour;
+				m[key].min = t.min;
+				m[key].isIn = false;
+			}
+			else
+			{
+				m[key].hour = t.hour;
+				m[key].min = t.min;
+				m[key].isIn = true;
+			}
+		}
+
+	}
+
+	for (auto& a : m)
+	{
+		if (a.second.isIn == true)
+		{
+			int vH = 23 - a.second.hour;
+			int vM = 59  -a.second.min;
+			int fV = vH * 60 + vM;
+			a.second.fullMin += fV;
+			a.second.isIn = false;
+		}
+
+		int r = fees[1]; // 기본시간
+		
+		int remainMin = a.second.fullMin;
+
+		if (remainMin <= fees[0])
+		{
+			answer.push_back(r);
+			continue;
+		}
+
+		remainMin -= fees[0];
+		int originRemain = remainMin;
+		remainMin /= fees[2];
+		if(originRemain % fees[2] != 0)
+			remainMin++;
+
+		r += remainMin * fees[3];
+
+		answer.push_back(r);
+	}
+
+	return answer;
+}
