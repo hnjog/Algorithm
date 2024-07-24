b4ecec3f431bf154cda81ef7b2ada8cbcc27a84e
hnjog <43630972+hnjog@users.noreply.github.com>
2024-07-24 13:04:24 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s) {
+	string answer = "";
+	bool isFirst = true;
+	for (auto c : s)
+	{
+		if (isFirst &&
+			c >= 'a' && c <= 'z')
+		{
+			c -= 32;
+
+			isFirst = false;
+		}
+	else if (isFirst == false &&
+		c >= 'A' && c <= 'Z')
+	{
+		c += 32;
+		isFirst = false;
+	}
+		else if (c == ' ')
+		{
+			isFirst = true;
+		}
+		else
+		{
+			isFirst = false;
+		}
+
+		answer += c;
+	}
+
+	return answer;
+}
