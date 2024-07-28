c68c7054e1c4c0215b7dcbd7e236fcadfa9b036b
hnjog <43630972+hnjog@users.noreply.github.com>
2024-07-28 09:11:24 +0900

    [level 2] Title: 귤 고르기, Time: 41.37 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+int solution(int k, vector<int> tangerine) {
+    int answer = 0;
+    map<int, int> m;
+
+    for (int t : tangerine)
+    {
+        if (m.find(t) == m.end())
+            m[t] = 0;
+
+        m[t] += 1;
+    }
+
+    vector<pair<int, int>> v(m.begin(), m.end());
+
+    sort(v.begin(), v.end(), [](const auto& a, const auto& b) {
+        return a.second > b.second;
+        });
+
+    int sum = 0;
+
+    for (auto p : v)
+    {
+        answer++;
+        sum += p.second;
+        if (sum >= k)
+            break;
+    }
+
+    return answer;
+}
