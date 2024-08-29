4576a7ea927d5f3e95e19882318b27a1bd8406f1
hnjog <43630972+hnjog@users.noreply.github.com>
2024-08-30 08:08:31 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+
+    while (n != 0)
+    {
+        if (n % 2 != 0)
+        {
+            answer++;
+            n--;
+        }
+
+        n /= 2;
+    }
+
+    int c = 0;
+
+    return answer;
+}
