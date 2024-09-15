864f8c5853defe0cfbbc3a58497d99ea9e361d28
hnjog <43630972+hnjog@users.noreply.github.com>
2024-09-16 07:43:28 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer(num_list.begin(),num_list.end());
+    
+    if(answer[answer.size() - 2] < answer[answer.size() - 1])
+    {
+        answer.push_back(answer[answer.size() - 1] - answer[answer.size() - 2]);
+    }
+    else
+    {
+        answer.push_back(answer.back() * 2);
+    }
+    
+    return answer;
+}
