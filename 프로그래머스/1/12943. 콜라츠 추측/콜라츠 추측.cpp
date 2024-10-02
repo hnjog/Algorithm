6982162021b3148861e1dfc81ba77eb5f66c7fde
hnjog <43630972+hnjog@users.noreply.github.com>
2024-10-03 08:52:33 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+
+    while (n != 1 &&
+        answer < 500)
+    {
+        if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n *= 3;
+            n++;
+        }
+
+        answer++;
+    }
+
+    if (answer >= 500)
+        return -1;
+
+    return answer;
+}
