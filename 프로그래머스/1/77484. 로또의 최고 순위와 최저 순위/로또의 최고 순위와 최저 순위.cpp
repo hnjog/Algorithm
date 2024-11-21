6625fbe5d29b638ba7ad26f768de215f167548dc
hnjog <43630972+hnjog@users.noreply.github.com>
2024-11-22 08:14:04 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include<set>
+#include<map>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer(2,6);
+    
+    set<int> lSet;
+    int zeroCount = 0;
+    
+    map<int,int> rank;
+    rank[6] = 1;
+    rank[5] = 2;
+    rank[4] = 3;
+    rank[3] = 4;
+    rank[2] = 5;
+    rank[1] = 6;
+    rank[0] = 6;
+    
+    for(int l : lottos)
+    {
+        if(l == 0)
+            zeroCount++;
+        
+        lSet.insert(l);
+    }
+    
+    int baseCount = 0;
+    
+    for(int w : win_nums)
+    {
+        if(lSet.find(w) != lSet.end())
+            baseCount++;
+    }
+    
+    if(baseCount == 6)
+    {
+        answer[0] = 1;
+        answer[1] = 1;
+    }
+    else
+    {
+        answer[1] = rank[baseCount];
+    }
+    
+    if(zeroCount != 0)
+    {
+        answer[0] = rank[baseCount + zeroCount];
+    }
+    else
+    {
+        answer[0] = answer[1];
+    }
+    
+    return answer;
+}
