991cdf4325c91f22680814bc8c37894acc1f2586
hnjog <43630972+hnjog@users.noreply.github.com>
2024-12-05 08:09:46 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.24 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,24 @@
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    
+    priority_queue<int,vector<int>,greater<int>> pq;
+    
+    for(int s : score)
+    {
+        pq.push(s);
+        
+        if(pq.size() > k)
+        {
+            pq.pop();
+        }
+        
+        answer.push_back(pq.top());
+    }
+    
+    return answer;
+}
