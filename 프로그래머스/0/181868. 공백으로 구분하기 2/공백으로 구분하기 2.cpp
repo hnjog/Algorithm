263a558cdea1d05cda37579b2a7eb2b698cd4d50
hnjog <43630972+hnjog@users.noreply.github.com>
2024-12-10 10:36:57 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.11 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    stringstream ss(my_string);
+    string temp;
+    
+    while(ss >> temp)
+    {
+        answer.push_back(temp);
+    }
+    
+    return answer;
+}
