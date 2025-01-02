ccda78a2ad79686928453ccd718e33ff113beb65
hnjog <43630972+hnjog@users.noreply.github.com>
2025-01-03 08:34:32 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    if(ineq == ">")
+    {
+        if(eq == "=")
+        {
+            return n >= m;
+        }
+        else
+        {
+            return n > m;
+        }
+    }
+    
+    if(eq == "=")
+        return n <= m;
+            
+    return n < m;
+}
