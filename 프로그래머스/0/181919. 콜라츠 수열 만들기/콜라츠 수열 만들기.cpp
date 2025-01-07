e73c0bf47085e3a5aa24c3db96429ae3437f2520
hnjog <43630972+hnjog@users.noreply.github.com>
2025-01-08 07:37:43 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    while(n != 1)
+    {
+        answer.push_back(n);
+        if(n%2 == 0)
+            n/=2;
+        else
+            n = n * 3 + 1;
+    }
+    answer.push_back(n);
+    return answer;
+}
