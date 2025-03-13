48f332bba9df63a820b93e733bd79fa57fbef3ce
hnjog <43630972+hnjog@users.noreply.github.com>
2025-03-14 08:33:24 +0900

    [Silver V] Title: 타일 장식물, Time: 0 ms, Memory: 2020 KB, Score: 74 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+typedef unsigned long long ull;
+
+ull dp[81] = {0,};
+
+int func(int n)
+{
+	if (n <= 0)
+		return 0;
+
+	if (dp[n] != 0)
+		return dp[n];
+
+	return dp[n] = func(n - 1) + func(n - 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	dp[1] = dp[2] = 1;
+
+	ull a = func(n);
+	ull b = func(n-1) + a;
+
+	cout << a * 2 + b * 2;
+}
