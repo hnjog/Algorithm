487f0f86031201eea18c71d20854b2de6ae72153
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-17 08:59:12 +0900

    [Gold V] Title: Moo 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/5904. Moo 게임/Moo 게임.cc
+++ 백준/Gold/5904. Moo 게임/Moo 게임.cc
@@ -6,20 +6,6 @@ using namespace std;
 
 const char cv[2] = { 'm','o' };
 
-// m과 o로 이루어짐
-// s0 : moo
-// s1 : s0 + moo + o + s0
-// s2 : s1 + moo + oo + s1
-// s3 : s2 + moo + ooo + s2
-// ...
-// sN : sN-1 + moo + o * n-1 + sN-1
-// 
-// 요점
-// - n이 속한 단계를 구함
-// - 분할 정복을 통하여 n이 어떠한 문자인지 구함
-// 
-//
-
 void isM(vector<int>& sizes, int step, int n)
 {
 	if (step == 0)
