c25aef1c0e06178efc796ec65786f8cd33640061
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-19 08:25:47 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -1,35 +1,34 @@
 #include <string>
 #include <vector>
-#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    // stack
-    stack<string> st1, st2;
-    for(int i= cards1.size() - 1; i >= 0;i--)
-    {
-        st1.push(cards1[i]);
-    }
-    
-    for(int i= cards2.size() - 1; i >= 0;i--)
-    {
-        st2.push(cards2[i]);
-    }
-    
     bool isS = true;
     
-    for(int i = 0; i < goal.size();i++)
+    queue<string> s1,s2;
+    for(string& s : cards1)
     {
-        string g = goal[i];
-        
-        if(st1.empty() == false && g == st1.top())
+        s1.push(s);
+    }
+    
+    for(string& s : cards2)
+    {
+        s2.push(s);
+    }
+    
+    for(auto it = goal.begin(); it != goal.end();it++)
+    {
+        if(s1.empty() == false && (*it) == s1.front())
         {
-            st1.pop();
+            s1.pop();
         }
-        else if(st2.empty() == false && g == st2.top())
+        else if(s2.empty() == false && (*it) == s2.front())
         {
-            st2.pop();
+            s2.pop();
         }
         else
         {
