0290d12111f05c9b3ebe08def7c5e29c917f7293
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-26 10:38:24 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,44 +1,31 @@
-#include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-
-    int pSize = progresses.size();
-    vector<pair<int, int>> days;
-
-    for (int i = 0; i < pSize; i++)
+    
+    int day = 1;
+    int pIdx = 0;
+    
+    while(pIdx < progresses.size())
     {
-        int remain = (100 - progresses[i]);
-        int day = remain / speeds[i];
-        if (remain % speeds[i] != 0)
+        int result = 0;
+        while(pIdx < progresses.size() &&
+            progresses[pIdx] + speeds[pIdx] * day >= 100)
         {
-            day++;
+            result++;
+            pIdx++;
         }
-
-        if (days.size() == 0)
+        
+        if(result != 0)
         {
-            days.push_back(make_pair(day, 1));
-            continue;
+            answer.push_back(result);
         }
-
-        if (day <= days.back().first)
-        {
-            days.back().second++;
-        }
-        else
-        {
-            days.push_back(make_pair(day, 1));
-        }
+        
+        day++;
     }
-
-    for (auto day : days)
-    {
-        answer.push_back(day.second);
-    }
-
+    
+    
     return answer;
 }
