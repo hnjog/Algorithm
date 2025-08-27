460ddb5eb86b403c8ca7d773889425b57ea2ae7f
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-27 09:11:40 +0900

    [level 2] Title: 타겟 넘버, Time: 5.83 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,24 +3,25 @@
 
 using namespace std;
 
-// 재귀를 통해 
-// target 값에 도달하는지 확인하기
+int answer = 0;
 
-int recur(int currentValue, const int targetValue, int index, const vector<int>& numbers)
+void recur(const vector<int>& numbers, const int target, int nowIdx, int nowValue)
 {
-	if (index == numbers.size())
-	{
-		if (currentValue == targetValue)
-			return 1;
-
-		return 0;
-	}
-
-	return recur(currentValue + numbers[index], targetValue, index + 1, numbers) + recur(currentValue - numbers[index], targetValue, index + 1, numbers);
+    int nSize = numbers.size();
+    if(nowIdx == nSize)
+    {
+        if(nowValue == target)
+            answer++;
+        
+        return;
+    }
+    
+    recur(numbers,target,nowIdx + 1,nowValue + numbers[nowIdx]);
+    recur(numbers,target,nowIdx + 1,nowValue - numbers[nowIdx]);
 }
 
-int solution(vector<int> numbers, int target) {
-	int answer = recur(0,target,0,numbers);
 
-	return answer;
+int solution(vector<int> numbers, int target) {
+    recur(numbers,target,0,0);
+    return answer;
 }
