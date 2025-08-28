5a2710c86069d8ba10475fed26106aff5ec2d8f2
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-28 09:53:24 +0900

    [level 2] Title: 롤케이크 자르기, Time: 60.10 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,17 +1,17 @@
-#include <map>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<int> topping) {
     int answer = 0;
-    map<int,int> rightMap;
+    unordered_map<int,int> rightMap;
     for(int i = 0; i <topping.size();i++)
     {
         rightMap[topping[i]]++;
     }
     
-    map<int,int> leftMap;
+    unordered_map<int,int> leftMap;
     for(int i = 0; i < topping.size();i++)
     {
         leftMap[topping[i]]++;
