fb7b04b467166181ee547283527848454073fdef
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-28 09:40:19 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 376.44 ms, Memory: 240 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -1,36 +1,28 @@
+#include <stack>
 #include <vector>
-#include<stack>
 
 using namespace std;
 
-typedef pair<int, int> pii;
-
 vector<int> solution(vector<int> numbers) {
-	const int nSize = numbers.size();
-	vector<int> answer(nSize, -1);
-
-	// 자기 값과 인덱스 넣어놓을 생각
-	stack<pii> st;
-
-	for (int i = 0; i < nSize; i++)
-	{
-		int nowNum = numbers[i];
-		while (st.empty() == false)
-		{
-			auto p = st.top();
-			if (nowNum > p.first)
-			{
-				st.pop();
-				answer[p.second] = numbers[i];
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		st.push({ nowNum,i });
-	}
-
-	return answer;
+    vector<int> answer(numbers.size());
+    stack<pair<int,int>> st;
+    for(int i = 0; i <numbers.size();i++)
+    {
+        while(st.empty() == false &&
+             st.top().second < numbers[i])
+        {
+            answer[st.top().first] = numbers[i];
+            st.pop();
+        }
+        
+        st.push({i,numbers[i]});
+    }
+    
+    while(st.empty() == false)
+    {
+        answer[st.top().first] = -1;
+        st.pop();
+    }
+    
+    return answer;
 }
