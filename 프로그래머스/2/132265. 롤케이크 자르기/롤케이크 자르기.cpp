715f1f00fce594ff5cd9da856f46b9f9bbf4ec46
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-28 09:52:16 +0900

    [level 2] Title: 롤케이크 자르기, Time: 210.09 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,45 +1,27 @@
+#include <map>
 #include <vector>
-#include <unordered_map>
 
 using namespace std;
 
-
 int solution(vector<int> topping) {
     int answer = 0;
-    
-    unordered_map<int, int> rightMap;
-
-    for (int i = 0; i < topping.size(); i++)
+    map<int,int> rightMap;
+    for(int i = 0; i <topping.size();i++)
     {
-        if (rightMap.find(topping[i]) == rightMap.end())
-            rightMap[topping[i]] = 0;
-
         rightMap[topping[i]]++;
     }
-
-    unordered_map<int, int> leftMap;
-
-    for (int i = 0; i < topping.size(); i++)
+    
+    map<int,int> leftMap;
+    for(int i = 0; i < topping.size();i++)
     {
-        int topp = topping[i];
-
-        if (leftMap.find(topp) == leftMap.end())
-            leftMap[topp] = 0;
-
-        leftMap[topp]++;
-
-        if (rightMap.find(topp) != rightMap.end())
-        {
-            rightMap[topp]--;
-            if (rightMap[topp] == 0)
-            {
-                rightMap.erase(topp);
-            }
-        }
-
-        if (leftMap.size() == rightMap.size())
+        leftMap[topping[i]]++;
+        rightMap[topping[i]]--;
+        if(rightMap[topping[i]] == 0)
+            rightMap.erase(topping[i]);
+        
+        if(leftMap.size() == rightMap.size())
             answer++;
     }
-
+    
     return answer;
 }
