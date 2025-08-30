a0932302f9289a0e3c51cd1ecd8b99dc811e3f66
hnjog <43630972+hnjog@users.noreply.github.com>
2025-08-30 11:02:19 +0900

    [level 2] Title: 소수 찾기, Time: 0.87 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -6,8 +6,30 @@
 
 using namespace std;
 
-void startRecur(string num, int size, set<int>& s);
-void recur(const string& num, string& temp, bool* bArr, int size, set<int>& s);
+void recur(const string& num,string& temp,vector<bool>& bVisit,int size, set<int>& s)
+{
+    if (temp.size() == size)
+    {
+        int r = stoi(temp);
+        s.insert(r);
+        return;
+    }
+    
+    int nSize = num.size();
+
+    for (int i = 0; i < nSize; i++)
+    {
+        // 이미 탐색이 완료된 부분
+        if (bVisit[i] == true)
+            continue;
+
+        bVisit[i] = true;
+        temp.push_back(num[i]);
+        recur(num,temp, bVisit,size,s);
+        temp.pop_back();
+        bVisit[i] = false;
+    }
+}
 
 bool isPrime(int n)
 {
@@ -32,11 +54,13 @@ int solution(string numbers) {
     int answer = 0;
 
     int size = numbers.size();
+    vector<bool> bVisit(size,false);
     set<int> s;
+    string temp = "";
 
     for (int i = 1; i <= size; i++)
     {
-        startRecur(numbers, i, s);
+        recur(numbers,temp, bVisit,i,s);
     }
 
     // 여기서 s 돌면서 체크하기
@@ -46,58 +70,6 @@ int solution(string numbers) {
             answer++;
     }
 
-
     return answer;
 }
 
-// 재귀 시작 용
-void startRecur(string num, int size, set<int>& s)
-{
-    // num의 각 요소들을 시작으로 for문을 돌리고
-    // 방문 관련 bool 배열도 이쪽에서 관리한다
-    int nSize = num.size();
-    bool* bVisit = new bool[nSize];
-    memset(bVisit, 0, sizeof(bool) * nSize);
-    
-    string temp = "";
-
-    for (int i = 0; i < nSize; i++)
-    {
-        // 여기서 처음 시작하는 부분에 bVisit를 true로 만들어 주고 아래쪽 함수에 넘겨주기
-        bVisit[i] = true;
-        temp += num[i];
-        recur(num,temp, bVisit,size,s);
-        bVisit[i] = false;
-        temp = "";
-    }
-
-    delete[] bVisit;
-}
-
-void recur(const string& num,string& temp,bool* bArr,int size, set<int>& s)
-{
-    // size와 num의 길이가 같으면 break 하면서 return 해야 한다
-    // 그리고 나서 check
-    if (temp.size() == size)
-    {
-        int r = stoi(temp);
-        s.insert(r);
-        return;
-    }
-    
-    int nSize = num.size();
-
-    for (int i = 0; i < nSize; i++)
-    {
-        // 이미 탐색이 완료된 부분
-        if (bArr[i] == true)
-            continue;
-
-        bArr[i] = true;
-        string originTemp = temp;
-        temp += num[i];
-        recur(num, temp, bArr, size, s);
-        temp = originTemp;
-        bArr[i] = false;
-    }
-}
