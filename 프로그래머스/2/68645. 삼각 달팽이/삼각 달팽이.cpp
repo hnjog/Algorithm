4ff987939fb44ace64877e48169d1f5cbc0d3048
hnjog <43630972+hnjog@users.noreply.github.com>
2025-09-02 09:25:30 +0900

    [level 2] Title: 삼각 달팽이, Time: 170.83 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -2,59 +2,50 @@
 
 using namespace std;
 
-vector<int> solution(int n) {
-	vector<int> answer;
-	
-	int count = 0;
-	for (int i = 1; i <= n; i++)
-		count += i;
+const int dirY[3] = {1,0,-1};
+const int dirX[3] = {0,1,-1};
 
-	int dx[3] = { 0,1,-1 };
-	int dy[3] = { 1,0,-1 };
-	vector<vector<int>> map(n, vector<int>(n, 0));
-
-	int dir = 0;
-	int value = 1;
-	int x = 0;
-	int y = 0;
-
-	while (true)
-	{
-		map[y][x] = value;
-
-		if (value >= count)
-			break;
-
-		int nextX = x + dx[dir];
-		int nextY = y + dy[dir];
-
-		if (nextX >= n || nextY >= n ||
-			nextX < 0 || nextY < 0 ||
-			map[nextY][nextX] != 0)
-		{
-			dir++;
-			if (dir >= 3)
-				dir = 0;
-
-			continue;
-		}
-
-		x = nextX;
-		y = nextY;
-
-		value++;
-	}
-
-	for (int i = 0; i < n; i++) // y
-	{
-		for (int j = 0; j < n; j++) // x
-		{
-			if (map[i][j] == 0)
-				break;
-
-			answer.push_back(map[i][j]);
-		}
-	}
-
-	return answer;
+vector<int> solution(int n) 
+{
+    vector<int> answer;
+    
+    int count = 0;
+    for(int i =0;i<=n;i++)
+        count +=i;
+    
+    vector<vector<int>> maps(n,vector<int>(n,0));
+    
+    int y = 0;
+    int x = 0;
+    int dir = 0;
+    int value = 0;
+    
+    while(true)
+    {
+        value++;
+        maps[y][x] = value;
+        if(value >= count)
+            break;
+        
+        int nextY = y + dirY[dir];
+        int nextX = x + dirX[dir];
+        
+        if(nextY < 0 || nextX < 0 ||
+          nextY >= n || nextX >= n ||
+          maps[nextY][nextX] != 0)
+        {
+            dir = (dir + 1) % 3;
+            nextY = y + dirY[dir];
+            nextX = x + dirX[dir];
+        }
+        
+        y = nextY;
+        x = nextX;
+    }
+    
+    for(int i = 0; i < n;i++)
+        for(int j = 0; j <=i;j++)
+            answer.push_back(maps[i][j]);
+    
+    return answer;
 }
