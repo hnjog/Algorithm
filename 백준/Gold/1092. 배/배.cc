87c00db92634dcc05c2375d5dbbdb957311d2ae1
hnjog <43630972+hnjog@users.noreply.github.com>
2025-09-18 09:10:52 +0900

    [Gold V] Title: 배, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.cc
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+
+using namespace std;
+
+int main()
+{
+	// 크레인과 박스들이 주어짐
+	// 
+	// 크레인은 자신보다 무거운 박스는 들지 못함
+	// 
+	// 박스를 그룹화 시키는 것은?
+	// 
+	// 1 크레인이 들 수 있는 박스
+	// 2 크레인이 들 수 있는 박스?
+	// 
+	// 박스들이 각각 자신이 들 수 있는 무거운 박스를 들어야 하는것은 맞음
+	// 
+	// 
+	//
+
+	int n;
+	cin >> n;
+	vector<int> crains(n);
+	for (int i = 0; i < n; i++)
+		cin >> crains[i];
+
+	sort(crains.begin(), crains.end());
+
+	int m;
+	cin >> m;
+	vector<int> boxs(m);
+	for (int i = 0; i < m; i++)
+		cin >> boxs[i];
+
+	sort(boxs.begin(), boxs.end());
+
+	if (boxs.back() > crains.back())
+	{
+		cout << -1;
+		return 0;
+	}
+
+	vector<int> idxs(n);
+	vector<bool> movedBox(m,false);
+
+	int idx = 0;
+	for (int i = 0; i < m; i++)
+	{
+		while(boxs[i] > crains[idx])
+		{
+			idxs[idx] = i - 1;
+			idx++;
+		}
+	}
+
+	if (idx < n)
+	{
+		for (int i = idx; i < n; i++)
+		{
+			idxs[i] = m - 1;
+		}
+	}
+
+	idxs.back() = m - 1;
+
+	// 무게 무거운 녀석들부터 옮긴다
+	int answer = 0;
+
+	while (true)
+	{
+		bool allComplete = true;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (idxs[i] == -1)
+				continue;
+
+			bool cantMove = false;
+
+			while (movedBox[idxs[i]])
+			{
+				idxs[i]--;
+				if (idxs[i] < 0)
+				{
+					cantMove = true;
+					break;
+				}
+			}
+
+			if (cantMove)
+				continue;
+
+			movedBox[idxs[i]] = true;
+			allComplete = false;
+		}
+
+		if (allComplete)
+			break;
+
+		answer++;
+	}
+
+	cout << answer;
+
+	return 0;
+}
