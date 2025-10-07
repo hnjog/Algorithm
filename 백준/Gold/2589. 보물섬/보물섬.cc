6544a61cf98e929af4f24608d45b89ad16f5768f
hnjog <43630972+hnjog@users.noreply.github.com>
2025-10-07 10:24:05 +0900

    [Gold V] Title: 보물섬, Time: 200 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -24,51 +24,6 @@ struct edge
 	int y, x, cost;
 };
 
-int find(vector<string>& maps, int nowY, int nowX)
-{
-	vector<vector<bool>> visit(h, vector<bool>(w, false));
-
-	queue<edge> q;
-
-	q.push({ nowY,nowX,0 });
-	int ret = 0;
-
-	visit[nowY][nowX] = true;
-
-	while (q.empty() == false)
-	{
-		int ny = q.front().y;
-		int nx = q.front().x;
-		int ncost = q.front().cost;
-		q.pop();
-
-		if (ncost > ret)
-		{
-			ret = ncost;
-		}
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nextY = ny + dirY[i];
-			int nextX = nx + dirX[i];
-
-			if (checkRightArrange(nextY, nextX) == false)
-				continue;
-
-			if (maps[nextY][nextX] == 'W')
-				continue;
-
-			if (visit[nextY][nextX])
-				continue;
-
-			visit[nextY][nextX] = true;
-			q.push({ nextY,nextX,ncost + 1 });
-		}
-	}
-
-	return ret;
-}
-
 int bfs(vector<string>& maps, int nowY, int nowX)
 {
 	if (checkRightArrange(nowY, nowX) == false)
@@ -120,15 +75,6 @@ int bfs(vector<string>& maps, int nowY, int nowX)
 			check[nextY][nextX] = true;
 			q.push({ nextY,nextX,ncost + 1 });
 		}
-	}
-
-	ret = 0;
-
-	for (auto& p : vValues)
-	{
-		int t = find(maps, p.first, p.second);
-		if (t > ret)
-			ret = t;
 	}
 
 	return ret;
