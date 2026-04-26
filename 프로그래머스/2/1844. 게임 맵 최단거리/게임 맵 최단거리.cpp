664f8161e499bba1dc82fb50841d64ca52e6ed86
hnjog <43630972+hnjog@users.noreply.github.com>
2026-04-27 08:12:03 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.39 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -4,68 +4,57 @@
 
 using namespace std;
 
-typedef pair<int, int> pii;
+const int dirY[4] = { 0,-1,0,1 };
+const int dirX[4] = { -1,0,1,0 };
 
-int solution(vector<vector<int> > maps)
+struct pos
 {
-	// 0,0 부터 maps(m x n)의 m,n 위치에 도달하려는 가장 짧은 크기
-	// 도달 불가 시 -1
+	int y, x, cost;
+};
+
+int solution(vector<vector<int>> maps)
+{
 	int answer = -1;
 
-	const pii dir[4] = {
-	{ 1,0},	// right
-	{ -1,0,},	// left
-	{ 0,1, },// down
-	{ 0,-1 }// up
-	};
+	int h = maps.size();
+	int w = maps[0].size();
 
-	int height = maps.size();
-	int width = maps[0].size();
+	vector<vector<int>> visited(h, vector<int>(w, INT_MAX));
 
-	struct loadInfos
-	{
-		int x = 0;
-		int y = 0;
-		int cost = 0;
-		loadInfos(int x,int y, int cost) : x(x),y(y),cost(cost) {}
-	};
-
-	// bfs 이기에 queue로 풀 예정
-	queue<loadInfos> q;
-	q.push(loadInfos(0,0,0));
+	queue<pos> q;
+	q.push({ 0,0,1 });
 
 	while (q.empty() == false)
 	{
-		loadInfos l = q.front();
+		int nowy = q.front().y;
+		int nowx = q.front().x;
+		int nowc = q.front().cost;
 		q.pop();
 
-		// 범위를 넘어섬
-		if (l.x >= width || l.x < 0 ||
-			l.y >= height || l.y < 0)
-			continue;
-
-		// 해당 길은 막혀 있음
-		if (maps[l.y][l.x] == 0)
-			continue;
-
-		if (l.x == width - 1 && l.y == height - 1)
+		if (nowy == h - 1 &&
+			nowx == w - 1)
 		{
-			if (answer == -1)
-			{
-				answer = INT_MAX;
-			}
-
-			answer = min(answer, l.cost + 1);
-			continue;
+			answer = nowc;
+			break;
 		}
 
-		maps[l.y][l.x] = 0; // visited 대신 이걸 사용하기 - bfs 탐색은 항상 최단 거리를 찾을 수 있으므로
+		if (nowy < 0 || nowy >= h ||
+			nowx < 0 || nowx >= w)
+			continue;
+
+		if (maps[nowy][nowx] == 0)
+			continue;
+
+		if (nowc >= visited[nowy][nowx])
+			continue;
+
+		visited[nowy][nowx] = nowc;
 
 		for (int i = 0; i < 4; i++)
 		{
-			const pii& d = dir[i];
-
-			q.push(loadInfos(l.x + d.first, l.y + d.second, l.cost + 1));
+			int ny = nowy + dirY[i];
+			int nx = nowx + dirX[i];
+			q.push({ ny,nx,nowc + 1 });
 		}
 	}
 
