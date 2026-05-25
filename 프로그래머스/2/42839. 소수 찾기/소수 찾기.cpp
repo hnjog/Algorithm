e4052555e43c7da1b434084ca4844a88475cf0e1
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-25 16:53:14 +0900

    [level 2] Title: 소수 찾기, Time: 192.74 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,75 +1,63 @@
 #include <string>
+#include <unordered_set>
 #include <vector>
-#include<math.h>
-#include<memory.h>
-#include<set>
 
 using namespace std;
 
-void recur(const string& num,string& temp,vector<bool>& bVisit,int size, set<int>& s)
+bool IsPrime(int value)
 {
-    if (temp.size() == size)
-    {
-        int r = stoi(temp);
-        s.insert(r);
-        return;
-    }
-    
-    int nSize = num.size();
-
-    for (int i = 0; i < nSize; i++)
-    {
-        // 이미 탐색이 완료된 부분
-        if (bVisit[i] == true)
-            continue;
-
-        bVisit[i] = true;
-        temp.push_back(num[i]);
-        recur(num,temp, bVisit,size,s);
-        temp.pop_back();
-        bVisit[i] = false;
-    }
-}
-
-bool isPrime(int n)
-{
-    if (n < 2)
+    if(value <= 1)
         return false;
-
-    if (n < 4)
+    
+    if(value < 4)
         return true;
-
-    int sqn = sqrt(n);
-
-    for (int i = 2; i <= sqn + 1; i++)
+    
+    for(int i = 2; i <= value / 2;i++)
     {
-        if (n % i == 0)
+        if(value % i == 0)
             return false;
     }
-
+    
     return true;
 }
 
-int solution(string numbers) {
-    int answer = 0;
-
-    int size = numbers.size();
-    vector<bool> bVisit(size,false);
-    set<int> s;
-    string temp = "";
-
-    for (int i = 1; i <= size; i++)
+void dfs(const string& origin, vector<bool>& visited,int now, string& temp,unordered_set<int>& us)
+{
+    if(temp.size() > 0)
     {
-        recur(numbers,temp, bVisit,i,s);
+        int s = stoi(temp);
+        if(IsPrime(s))
+        {
+            us.insert(s);
+        }    
     }
-
-    // 여기서 s 돌면서 체크하기
-    for (auto a : s)
+    
+    int oSize = origin.size();
+    
+    for(int i=0;i<oSize;i++)
     {
-        if (isPrime(a))
-            answer++;
+        if(visited[i])
+            continue;
+        
+        visited[i] = true;
+        temp.push_back(origin[i]);
+        dfs(origin,visited,i,temp,us);
+        temp.pop_back();
+        visited[i] = false;
     }
-
-    return answer;
 }
 
+int solution(string numbers) {
+    int best = 0;
+    unordered_set<int> us;
+    for(int i=0;i<numbers.size();i++)
+    {
+        vector<bool> visited(numbers.size());
+        string temp = "";
+        dfs(numbers,visited,i,temp,us);    
+        if(us.size() > best)
+            best = us.size();
+    }
+    
+    return best;
+}
