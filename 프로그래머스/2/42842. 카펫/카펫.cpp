8f6c3523b51ed5e48e06fa95a96a54e4302c6581
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-26 08:32:00 +0900

    [level 2] Title: 카펫, Time: 1.19 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,28 +1,32 @@
-#include <string>
 #include <vector>
 
 using namespace std;
+
+int GetBrown(int sum, int width)
+{
+    int h = sum / width;
+    return width * 2 + h * 2 - 4;
+}
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     int sum = brown + yellow;
 
-    // answer[0]의 범위는 1 ~ s / 2까지
-    int s = brown / 2 + 2;
-
-    for (int i = s / 2; i > 0; i--)
+    for (int i = sum / 2; i >= 3; i--)
     {
-        if (i * (s - i) == sum)
+        if (sum % i == 0 &&
+            GetBrown(sum, i) == brown)
         {
+            int j = sum / i;
+            if (i < j)
+            {
+                swap(i, j);
+            }
+
             answer.push_back(i);
-            answer.push_back(s - i);
+            answer.push_back(j);
             break;
         }
-    }
-
-    if (answer[0] < answer[1])
-    {
-        swap(answer[0], answer[1]);
     }
 
     return answer;
