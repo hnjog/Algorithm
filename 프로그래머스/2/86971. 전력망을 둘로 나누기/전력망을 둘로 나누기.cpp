4a80e92a97a1bf33c9c42f41b3652aa7ccd517bb
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-26 11:13:37 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.17 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,61 +1,68 @@
 #include <vector>
+#include <queue>
 #include <unordered_map>
-#include <limits.h>
 
 using namespace std;
 
-int dfs(unordered_map<int, vector<int>>& maps, vector<bool>& visit, int now,int notVisit)
+int GetValue(unordered_map<int, vector<int>>& maps, int start, vector<bool>& visit)
 {
-    if (now == notVisit)
-        return 0;
+    int ret = 0;
 
-    if (visit[now])
-        return 0;
+    queue<int> q;
+    q.push(start);
 
-    visit[now] = true;
-
-    int ret = 1;
-    for (int next : maps[now])
+    while (q.empty() == false)
     {
-        ret += dfs(maps, visit, next, notVisit);
+        int n = q.front();
+        q.pop();
+
+        if (visit[n] == true &&
+            n != start)
+        {
+            continue;
+        }
+
+        visit[n] = true;
+        ret++;
+
+        for (int next : maps[n])
+        {
+            if (visit[next])
+                continue;
+            q.push(next);
+        }
+
     }
 
     return ret;
 }
 
-int GetNodeCount(int n,unordered_map<int, vector<int>>& maps, int start, int notVisit)
-{
-    vector<bool> visit(n + 1);
-    return dfs(maps, visit, start, notVisit);
-}
-
 int solution(int n, vector<vector<int>> wires) {
-    int answer = INT_MAX;
-    // 그냥 wire 마다 짜르고
-    // 짜른 부위마다 dfs 돌려서
-    // 나온 개수 1 - 나온 개수 2 절댓값 구하기
-
+    int answer = n;
     unordered_map<int, vector<int>> maps;
 
-    for (auto& wire : wires)
+    for (auto& w : wires)
     {
-        int s = wire[0];
-        int t = wire[1];
-
-        maps[s].push_back(t);
-        maps[t].push_back(s);
+        int start = w[0] - 1;
+        int end = w[1] - 1;
+        maps[start].push_back(end);
+        maps[end].push_back(start);
     }
 
-    for (auto& wire : wires)
+
+    for (auto& w : wires)
     {
-        int v1 = GetNodeCount(n, maps, wire[0], wire[1]);
-        int v2 = GetNodeCount(n, maps, wire[1], wire[0]);
+        vector<bool> visit(n, false);
+        int start = w[0] - 1;
+        int end = w[1] - 1;
+        visit[start] = true;
+        visit[end] = true;
+        int v1 = GetValue(maps, start, visit);
+        int v2 = GetValue(maps, end, visit);
+        int vv = v1 - v2 < 0 ? v2 - v1 : v1 - v2;
 
-        if (v2 > v1)
-            swap(v1, v2);
-
-        if (answer > v1 - v2)
-            answer = v1 - v2;
+        if (vv < answer)
+            answer = vv;
     }
 
     return answer;
