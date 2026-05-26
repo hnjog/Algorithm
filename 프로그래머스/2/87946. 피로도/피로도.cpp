a45d81a6a55036a523cc2d099cac37e7b7d66cf2
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-26 09:45:15 +0900

    [level 2] Title: 피로도, Time: 0.54 ms, Memory: 4.38 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,49 +1,51 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
-void recur(const vector<vector<int>>& d, vector<bool>& bVec, int count, int result, int remainK, int& answer);
+void Recur(const vector<vector<int>>& dungeons, int now, int count, int remainK, vector<bool>& visit, int& answer)
+{
+    if (count > answer)
+    {
+        answer = count;
+    }
+
+    int dsize = dungeons.size();
+
+    if (count >= dsize)
+        return;
+
+    for (int i = 0; i < dsize; i++)
+    {
+        if (visit[i] == true)
+            continue;
+
+        if (dungeons[i][0] > remainK)
+            continue;
+
+        visit[i] = true;
+        Recur(dungeons, i, count + 1, remainK - dungeons[i][1], visit, answer);
+        visit[i] = false;
+    }
+}
+
+void StartRecur(const vector<vector<int>>& dungeons,int k, int& answer)
+{
+    int dsize = dungeons.size();
+    vector<bool> visit(dsize, false);
+
+    for (int i = 0; i < dsize; i++)
+    {
+        if (dungeons[i][0] > k)
+            continue;
+
+        visit[i] = true;
+        Recur(dungeons, i, 1, k - dungeons[i][1], visit, answer);
+        visit[i] = false;
+    }
+}
 
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = 0;
-    int dunSize = dungeons.size();
-    vector<bool> bVector(dunSize, false);
-
-    for (int i = 0; i < dunSize; i++)
-    {
-        if (k >= dungeons[i][0])
-        {
-            bVector[i] = true;
-            recur(dungeons, bVector, 1, 1, k - dungeons[i][1], answer);
-            bVector[i] = false;
-        }
-    }
-
+    StartRecur(dungeons,k, answer);
     return answer;
-}
-
-void recur(const vector<vector<int>>& d, vector<bool>& bVec, int count, int result, int remainK, int& answer)
-{
-    int dSize = d.size();
-    if (result > answer)
-    {
-        answer = result;
-        
-        if(count == dSize)
-            return;
-    }
-
-    for (int i = 0; i < dSize; i++)
-    {
-        if (bVec[i])
-            continue;
-
-        if (remainK >= d[i][0])
-        {
-            bVec[i] = true;
-            recur(d, bVec, count + 1, result + 1, remainK - d[i][1], answer);
-            bVec[i] = false;
-        }
-    }
 }
