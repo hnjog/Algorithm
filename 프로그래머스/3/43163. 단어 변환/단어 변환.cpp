23d784ebdcee7c68f46cd89206e7145accdcaa47
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-27 16:54:42 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,68 +1,69 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <unordered_set>
 
 using namespace std;
 
-bool IsOneDiffer(const string& a, const string& b)
+bool CanTrans(const string& a, const string& b)
 {
-	if (a.size() != b.size())
-		return false;
+    if (a.size() != b.size())
+        return false;
 
-	int differCount = 0;
+    int changeCount = 0;
 
-	int ss = a.size();
+    int ss = a.size();
+    for (int i = 0; i < ss; i++)
+    {
+        if (a[i] != b[i])
+            changeCount++;
 
-	for (int i = 0; i < ss; i++)
-	{
-		if (a[i] != b[i])
-		{
-			differCount++;
-		}
+        if (changeCount > 1)
+            return false;
+    }
 
-		if (differCount > 1)
-			break;
-	}
+    if (changeCount == 1)
+        return true;
 
-	if (differCount != 1)
-		return false;
-
-	return true;
+    return false;
 }
 
-void dfs(const string& now, const string& target, const vector<string>& words, unordered_set<string>& us, int nowC, int& answer)
+struct infos
 {
-	if (now == target)
-	{
-		if (answer == 0 ||
-			answer > nowC)
-		{
-			answer = nowC;
-		}
-
-		return;
-	}
-
-	us.insert(now);
-
-	for (const string& str : words)
-	{
-		if (us.find(str) != us.end())
-			continue;
-
-		if (IsOneDiffer(now, str) == true)
-		{
-			dfs(str, target, words, us, nowC + 1, answer);
-		}
-	}
-
-	us.erase(now);
-}
+    string str;
+    int cost;
+};
 
 int solution(string begin, string target, vector<string> words) {
-	int answer = 0;
-	unordered_set<string> us;
-	us.reserve(words.size());
-	dfs(begin, target, words, us, 0, answer);
-	return answer;
+    queue<infos> q;
+    unordered_set<string> visit;
+
+    q.push({ begin,0 });
+
+    while (q.empty() == false)
+    {
+        string now = q.front().str;
+        int nowcost = q.front().cost;
+        q.pop();
+
+        if (now == target)
+        {
+            return nowcost;
+        }
+
+        if (visit.find(now) != visit.end())
+            continue;
+
+        visit.insert(now);
+
+        for (const string& word : words)
+        {
+            if (CanTrans(now, word))
+            {
+                q.push({ word,nowcost + 1 });
+            }
+        }
+    }
+
+    return 0;
 }
