7a8450157047fc20261359ed78a216fcba0645df
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-28 12:14:08 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,45 +1,44 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
-typedef pair<int,int> pii;
+typedef pair<int, int> pii;
 
 int solution(vector<int> priorities, int location) {
     int answer = 1;
+
+    int n = priorities.size();
     queue<pii> q;
     
-    for(int i = 0; i < priorities.size();i++)
+    for (int i = 0; i < n; i++)
     {
-        q.push({priorities[i],i});
+        q.push({ priorities[i],i });
     }
-    
-    sort(priorities.begin(),priorities.end(),greater<int>());
-    int pIdx = 0;
-    
-    while(q.empty() == false)
+
+    sort(priorities.begin(), priorities.end(), greater<int>());
+
+    int pidx = 0;
+    while (q.empty() == false)
     {
         auto p = q.front();
         q.pop();
-        
-        if(p.first == priorities[pIdx])
+
+        if (p.first == priorities[pidx])
         {
-            pIdx++;
-            
-            if(p.second == location)
+            pidx++;
+
+            if (p.second == location)
                 break;
-            
+
             answer++;
-            
+
         }
         else
         {
             q.push(p);
         }
     }
-    
-    
     return answer;
 }
