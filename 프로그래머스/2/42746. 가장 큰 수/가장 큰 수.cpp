38bbf0c33d211cbf6645d01a73b3a30e60ea2b4e
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-29 13:22:00 +0900

    [level 2] Title: 가장 큰 수, Time: 48.81 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -6,21 +6,32 @@ using namespace std;
 
 string solution(vector<int> numbers) {
     string answer = "";
-
-    sort(numbers.begin(), numbers.end(), [](int a, int b) -> bool {
-	string sa = to_string(a);
-	string sb = to_string(b);
-
-	return sa + sb > sb + sa;
-	});
-
-    for (auto a : numbers)
+    int n = numbers.size();
+    vector<string> nums;
+    nums.reserve(n);
+    for (int i = 0; i < n; i++)
     {
-        if (answer == "0" && a == 0)
-	        continue;
-        
-        answer += to_string(a);
+        nums.push_back(to_string(numbers[i]));
     }
+
+    sort(nums.begin(), nums.end(), [](const string& a, const string& b)
+        {
+            string ab = a + b;
+            string ba = b + a;
+
+            if (ab == ba)
+                return a.size() > b.size();
+
+            return ab > ba;
+        });
+
+    for (string& str : nums)
+    {
+        answer.append(str);
+    }
+
+    if (answer[0] == '0')
+        answer = "0";
 
     return answer;
 }
