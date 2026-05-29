9722b3d1860c04a875fe1c8baa424ae6c8cf5667
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-29 11:46:26 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,32 +1,22 @@
-#include <string>
-#include <vector>
 #include <algorithm>
-
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    vector<int> temp;
-    
-    
-    for(const auto& v : commands)
+    for (vector<int>& com : commands)
     {
-        int start = v[0] - 1;
-        int end = v[1] - 1;
-        int target = v[2] - 1;
-        
-        for(int i = start; i <= end; i++)
+        int start = com[0] - 1;
+        int end = com[1] - 1;
+        int target = com[2] - 1;
+        vector<int> temp;
+        for (int i = start; i <= end; i++)
         {
             temp.push_back(array[i]);
         }
-        
-        sort(temp.begin(),temp.end());
-        
+        sort(temp.begin(), temp.end());
         answer.push_back(temp[target]);
-        temp.clear();
     }
-    
     return answer;
 }
