36498003d31f490ec90b3206c1797eb5623f154e
hnjog <43630972+hnjog@users.noreply.github.com>
2026-05-31 08:15:06 +0900

    [level 2] Title: 롤케이크 자르기, Time: 29.35 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,25 +1,25 @@
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 int solution(vector<int> topping) {
     int answer = 0;
-    unordered_map<int,int> rightMap;
-    for(int i = 0; i <topping.size();i++)
+    
+    unordered_map<int,int> um1,um2;
+    for(int t : topping)
     {
-        rightMap[topping[i]]++;
+        um2[t]++;
     }
     
-    unordered_map<int,int> leftMap;
-    for(int i = 0; i < topping.size();i++)
+    for(int i=0;i<topping.size();i++)
     {
-        leftMap[topping[i]]++;
-        rightMap[topping[i]]--;
-        if(rightMap[topping[i]] == 0)
-            rightMap.erase(topping[i]);
+        um1[topping[i]]++;
+        um2[topping[i]]--;
+        if(um2[topping[i]] == 0)
+            um2.erase(topping[i]);
         
-        if(leftMap.size() == rightMap.size())
+        if(um1.size() == um2.size())
             answer++;
     }
     
