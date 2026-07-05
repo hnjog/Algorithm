9becf13dcbdeb44c2f324172a61affb0d75bae09
hnjog <43630972+hnjog@users.noreply.github.com>
2026-07-06 08:28:37 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 4 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	
+	cin>>T;
+	
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		int a,b;
+        cin >> a >> b;
+        cout << '#' << test_case << ' ';
+        if(a < b)
+            cout << '<';
+        else if(a > b)
+            cout << '>';
+        else
+            cout << '=';
+        cout << '\n';
+	}
+	return 0;
+}
